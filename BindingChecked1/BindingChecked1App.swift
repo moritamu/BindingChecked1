702f5eb0b33ee21210deaca15d36e525430fb5cb
morisum <moris@r.sannet.ne.jp>
2024-01-17 17:35:35 +0900

    Initial Commit

---
+++ BindingChecked1/BindingChecked1App.swift
@@ -0,0 +1,17 @@
+//
+//  BindingChecked1App.swift
+//  BindingChecked1
+//
+//  Created by MsMacM on 2024/01/17.
+//
+
+import SwiftUI
+
+@main
+struct BindingChecked1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
