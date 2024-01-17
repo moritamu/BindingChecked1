b1fcca36aee7d2fde7fce046feb55df0e73ae302
morisum <moris@r.sannet.ne.jp>
2024-01-18 05:59:28 +0900

    チェックマークの構造体を作成 商品化

--- BindingChecked1/ContentView.swift
+++ BindingChecked1/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isMychecked1: Bool = false
+    @State var isMychecked2: Bool = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("担当者１のチェック")
+                CheckMarkPerson(isChecked: $isMychecked1)
+            }
+            HStack {
+                Text("担当者２のチェック")
+                CheckMarkPerson(isChecked: $isMychecked2)
+            }
+            .padding()
         }
-        .padding()
     }
 }
-
+//チェックマークViewの構造体
+struct CheckMarkPerson: View {
+    @Binding var isChecked: Bool
+    
+    var body: some View {
+        Button(action: {
+            self.isChecked.toggle()
+        }) {
+            Image(systemName: isChecked ?
+                  "person.crop.circle.badge.checkmark" :
+                    "person.crop.circle")
+            .foregroundColor(isChecked ? .blue : .red)
+        }
+        .imageScale(.large)
+        .frame(width: 40)
+    }
+}
 #Preview {
     ContentView()
 }
